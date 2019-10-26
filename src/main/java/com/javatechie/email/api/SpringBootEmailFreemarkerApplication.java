c51dcc79a362986e06b88345752c170135f9d301
jose antonio alvino velasque <jose_antonio_alvino21@outlook.es>
2019-10-26 01:04:53 -0500

    service email

---
+++ src/main/java/com/javatechie/email/api/SpringBootEmailFreemarkerApplication.java
@@ -0,0 +1,47 @@
+package com.javatechie.email.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatechie.email.api.dto.MailRequest;
+import com.javatechie.email.api.dto.MailResponse;
+//import com.javatechie.email.api.dto.ValidacionRegistroUsuario;
+import com.javatechie.email.api.service.EmailService;
+//import com.javatechie.email.api.service.ValidacionRegistroUsuarioService;
+
+@CrossOrigin(origins = "*")
+@SpringBootApplication
+@RestController
+public class SpringBootEmailFreemarkerApplication {
+
+	@Autowired
+	private EmailService service;
+	//private ValidacionRegistroUsuarioService serviceValidacion;
+
+	@PostMapping("/sendingEmail")
+	public MailResponse sendEmail(@RequestBody MailRequest request) {
+		Map<String, Object> model = new HashMap<>();
+		model.put("Name", request.getName());
+		model.put("location", "Bangalore,India");
+		return service.sendEmail(request, model);
+
+	}
+	/*
+	@PostMapping(path= {"/validacionregistro/"})
+	public ValidacionRegistroUsuario insertValidation(@RequestBody ValidacionRegistroUsuario valreg) {
+		
+		return serviceValidacion.add(valreg);
+	}
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmailFreemarkerApplication.class, args);
+	}
+}
